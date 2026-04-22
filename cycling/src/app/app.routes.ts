a89ee79f3f3abf89ae0fb8f2093067c94c35f676
Ewoud <ewoud.vandenheede@student.ucll.be>
2026-04-22 11:36:45 +0200

    Pagina toegevoegt om te kijken of components werken

--- cycling/src/app/app.routes.ts
+++ cycling/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { ChooseRace } from './choose-race/choose-race';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', redirectTo: 'ChooseRace', pathMatch: 'full' },
+    {path: 'ChooseRace', component: ChooseRace},
+];
