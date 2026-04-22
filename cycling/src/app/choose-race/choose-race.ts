36b2d703c5b6e3ae633d34b650f4503b9315e3f8
Ewoud <ewoud.vandenheede@student.ucll.be>
2026-04-22 11:28:44 +0200

    Added choose-race page

---
+++ cycling/src/app/choose-race/choose-race.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-choose-race',
+  imports: [],
+  templateUrl: './choose-race.html',
+  styleUrl: './choose-race.css',
+})
+export class ChooseRace {}
