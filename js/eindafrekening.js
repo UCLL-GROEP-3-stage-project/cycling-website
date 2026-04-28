7f6068efae933a1be6b9c81f584857524307352c
Ewoud <ewoud.vandenheede@student.ucll.be>
2026-04-28 15:42:46 +0200

    fixed math issue in score.

--- js/eindafrekening.js
+++ js/eindafrekening.js
@@ -24,55 +24,55 @@ fetch(base_url + "/algemeen_klassement.json")
             if (c.cyclist_id === cyclist.cyclist_id) {
 
                 if (cyclist.position == 1) {
-                    score += 300;
+                    score = 300;
                 } else if (cyclist.position == 2) {
-                    score += 240;
+                    score = 240;
                 } else if (cyclist.position == 3) {
-                    score += 195;
+                    score = 195;
                 } else if (cyclist.position == 4) {
-                    score += 165;
+                    score = 165;
                 } else if (cyclist.position == 5) {
-                    score += 135;
+                    score = 135;
                 } else if (cyclist.position == 6) {
-                    score += 105;
+                    score = 105;
                 } else if (cyclist.position == 7) {
-                    score += 90;
+                    score = 90;
                 } else if (cyclist.position == 8) {
-                    score += 75;
+                    score = 75;
                 } else if (cyclist.position == 9) {
-                    score += 60;
+                    score = 60;
                 } else if (cyclist.position == 10) {
-                    score += 51;
+                    score = 51;
                 } else if (cyclist.position == 11) {
-                    score += 45;
+                    score = 45;
                 } else if (cyclist.position == 12) {
-                    score += 42;
+                    score = 42;
                 } else if (cyclist.position == 13) {
-                    score += 39;
+                    score = 39;
                 } else if (cyclist.position == 14) {
-                    score += 36;
+                    score = 36;
                 } else if (cyclist.position == 15) {
-                    score += 33;
+                    score = 33;
                 } else if (cyclist.position == 16) {
-                    score += 30;
+                    score = 30;
                 } else if (cyclist.position == 17) {
-                    score += 27;
+                    score = 27;
                 } else if (cyclist.position == 18) {
-                    score += 24;
+                    score = 24;
                 } else if (cyclist.position == 19) {
-                    score += 21;
+                    score = 21;
                 } else if (cyclist.position == 20) {
-                    score += 18;
+                    score = 18;
                 } else if (cyclist.position == 21) {
-                    score += 15;
+                    score = 15;
                 } else if (cyclist.position == 22) {
-                    score += 12;
+                    score = 12;
                 } else if (cyclist.position == 23) {
-                    score += 9;
+                    score = 9;
                 } else if (cyclist.position == 24) {
-                    score += 6;
+                    score = 6;
                 } else if (cyclist.position == 25) {
-                    score += 3;
+                    score = 3;
                 }
 
                 c.score += score;
@@ -99,25 +99,25 @@ fetch(base_url + "/bergtrui.json")
             if (c.cyclist_id === cyclist.cyclist_id) {
 
                 if (cyclist.position == 1) {
-                    score += 100;
+                    score = 100;
                 } else if (cyclist.position == 2) {
-                    score += 75;
+                    score = 75;
                 } else if (cyclist.position == 3) {
-                    score += 50;
+                    score = 50;
                 } else if (cyclist.position == 4) {
-                    score += 40;
+                    score = 40;
                 } else if (cyclist.position == 5) {
-                    score += 30;
+                    score = 30;
                 } else if (cyclist.position == 6) {
-                    score += 25;
+                    score = 25;
                 } else if (cyclist.position == 7) {
-                    score += 20;
+                    score = 20;
                 } else if (cyclist.position == 8) {
-                    score += 15;
+                    score = 15;
                 } else if (cyclist.position == 9) {
-                    score += 10;
+                    score = 10;
                 } else if (cyclist.position == 10) {
-                    score += 5;
+                    score = 5;
                 }
 
 
@@ -147,25 +147,25 @@ fetch(base_url + "/jongerentrui.json")
             if (c.cyclist_id === cyclist.cyclist_id) {
 
                 if (cyclist.position == 1) {
-                    score += 100;
+                    score = 100;
                 } else if (cyclist.position == 2) {
-                    score += 75;
+                    score = 75;
                 } else if (cyclist.position == 3) {
-                    score += 50;
+                    score = 50;
                 } else if (cyclist.position == 4) {
-                    score += 40;
+                    score = 40;
                 } else if (cyclist.position == 5) {
-                    score += 30;
+                    score = 30;
                 } else if (cyclist.position == 6) {
-                    score += 25;
+                    score = 25;
                 } else if (cyclist.position == 7) {
-                    score += 20;
+                    score = 20;
                 } else if (cyclist.position == 8) {
-                    score += 15;
+                    score = 15;
                 } else if (cyclist.position == 9) {
-                    score += 10;
+                    score = 10;
                 } else if (cyclist.position == 10) {
-                    score += 5;
+                    score = 5;
                 }
 
 
@@ -202,25 +202,25 @@ fetch(base_url + "/puntentrui.json")
             if (c.cyclist_id === cyclist.cyclist_id) {
 
                 if (cyclist.position == 1) {
-                    score += 100;
+                    score = 100;
                 } else if (cyclist.position == 2) {
-                    score += 75;
+                    score = 75;
                 } else if (cyclist.position == 3) {
-                    score += 50;
+                    score = 50;
                 } else if (cyclist.position == 4) {
-                    score += 40;
+                    score = 40;
                 } else if (cyclist.position == 5) {
                     score += 30;
                 } else if (cyclist.position == 6) {
-                    score += 25;
+                    score = 25;
                 } else if (cyclist.position == 7) {
-                    score += 20;
+                    score = 20;
                 } else if (cyclist.position == 8) {
-                    score += 15;
+                    score = 15;
                 } else if (cyclist.position == 9) {
-                    score += 10;
+                    score = 10;
                 } else if (cyclist.position == 10) {
-                    score += 5;
+                    score = 5;
                 }
 
 
