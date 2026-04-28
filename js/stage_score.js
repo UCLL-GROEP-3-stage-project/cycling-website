1baa4fd141d607f94b05aec68df935cfc7f5dd9c
Ewoud <ewoud.vandenheede@student.ucll.be>
2026-04-28 14:39:47 +0200

    Fixed naming mistake in the data!

--- js/stage_score.js
+++ js/stage_score.js
@@ -1,3 +1,7 @@
+document.getElementById("nextStage").style.display = "none";
+document.getElementById("endScore").style.display = "none";
+
+
 //localstorage data ophalen
 let savedGame  = localStorage.getItem("game");
 
@@ -5,12 +9,9 @@ let game;
 
 if (savedGame) {
   game = JSON.parse(savedGame);
-  console.log("Game geladen:", game);
 
     //text inladen
     document.getElementById("stageText").textContent = "Stage: " + game.current_stage;
-
-
 } else {
   alert("Geen opgeslagen game gevonden");
 }
@@ -28,7 +29,7 @@ fetch(base_url + "/DNF.json")
         game.players.forEach(player => {
 
             const hasCyclist = player.list_active.some(p => 
-                p.cyclist_id === cyclist.cyclist_id
+                p.cyclist_id == cyclist.cyclist_id
             );
 
             if (hasCyclist) {
@@ -36,7 +37,6 @@ fetch(base_url + "/DNF.json")
 
                 console.log("Player " + player.name + " has cyclist " + cyclist.cyclist_id + " in their active list.");    
             } else {
-                //
                 console.log("Player " + player.name + " does NOT have cyclist " + cyclist.cyclist_id + " in their active list.");
             }
 
@@ -56,64 +56,75 @@ fetch(base_url + "/results.json")
 
     data.StageResult.forEach(cyclist => {
 
+        let score = 0;
+
         game.all_cyclists.forEach(c => {
-            if (c.cyclist_id === cyclist.cyclist_id) {
+            if (c.cyclist_id == cyclist.cyclist_id) {
 
                 if (cyclist.positie == 1) {
-                    c.score += 100;
-                } else if (cyclist.positie == 2) {
-                    c.score += 80;
-                } else if (cyclist.positie == 3) {
-                    c.score += 65;
-                } else if (cyclist.positie == 4) {
-                    c.score += 55;
-                } else if (cyclist.positie == 5) {
-                    c.score += 45;
-                } else if (cyclist.positie == 6) {
-                    c.score += 35;
-                } else if (cyclist.positie == 7) {
-                    c.score += 30;
-                } else if (cyclist.positie == 8) {
-                    c.score += 25;
-                } else if (cyclist.positie == 9) {
-                    c.score += 20;
-                } else if (cyclist.positie == 10) {
-                    c.score += 17;
-                } else if (cyclist.positie == 11) {
-                    c.score += 15;
-                } else if (cyclist.positie == 12) {
-                    c.score += 14;
-                } else if (cyclist.positie == 13) {
-                    c.score += 13;
-                } else if (cyclist.positie == 14) {
-                    c.score += 12;
-                } else if (cyclist.positie == 15) {
-                    c.score += 11;
-                } else if (cyclist.positie == 16) {
-                    c.score += 10;
-                } else if (cyclist.positie == 17) {
-                    c.score += 9;
-                } else if (cyclist.positie == 18) {
-                    c.score += 8;
-                } else if (cyclist.positie == 19) {
-                    c.score += 7;
-                } else if (cyclist.positie == 20) {
-                    c.score += 6;
-                } else if (cyclist.positie == 21) {
-                    c.score += 5;
-                } else if (cyclist.positie == 22) {
-                    c.score += 4;
-                } else if (cyclist.positie == 23) {
-                    c.score += 3;
-                } else if (cyclist.positie == 24) {
-                    c.score += 2;
-                } else if (cyclist.positie == 25) {
-                    c.score += 1;
+                    score = 100;
+                } else if (cyclist.position == 2) {
+                    score = 80;
+                } else if (cyclist.position == 3) {
+                    score = 65;
+                } else if (cyclist.position == 4) {
+                    score += 55;
+                } else if (cyclist.position == 5) {
+                    score = 45;
+                } else if (cyclist.position == 6) {
+                    score = 35;
+                } else if (cyclist.position == 7) {
+                    score = 30;
+                } else if (cyclist.position == 8) {
+                    score = 25;
+                } else if (cyclist.position == 9) {
+                    score = 20;
+                } else if (cyclist.position == 10) {
+                    score = 17;
+                } else if (cyclist.position == 11) {
+                    score = 15;
+                } else if (cyclist.position == 12) {
+                    score += 14;
+                } else if (cyclist.position == 13) {
+                    score += 13;
+                } else if (cyclist.position == 14) {
+                    score += 12;
+                } else if (cyclist.position == 15) {
+                    score += 11;
+                } else if (cyclist.position == 16) {
+                    score += 10;
+                } else if (cyclist.position == 17) {
+                    score += 9;
+                } else if (cyclist.position == 18) {
+                    score += 8;
+                } else if (cyclist.position == 19) {
+                    score += 7;
+                } else if (cyclist.position == 20) {
+                    score += 6;
+                } else if (cyclist.position == 21) {
+                    score += 5;
+                } else if (cyclist.position == 22) {
+                    score += 4;
+                } else if (cyclist.position == 23) {
+                    score += 3;
+                } else if (cyclist.position == 24) {
+                    score += 2;
+                } else if (cyclist.position == 25) {
+                    score += 1;
                 }
+
+                c.score += score;
+                game.players.forEach(player => {
+                    // De lijst waaruit ik pak moet wrs nog eens aangepast worden
+                    if (player.list_active.some(p => p.cyclist_id == cyclist.cyclist_id)) {
+                        player.list_score[game.current_stage - 1] += score;
+                    }
+                });
             }
         });
 
     });
+    updateButtons();
 
   })
   .catch(err => console.error("Error loading JSON:", err));
@@ -165,13 +176,18 @@ fetch(base_url + "/leaders.json")
 
 
 //Checken welke stage we zitten.
-if (game.current_stage == game.players[0].list_score.length) {
-    document.getElementById("nextStage").style.display = "none";
-    document.getElementById("endScore").style.display = "block";
-} else {
-    document.getElementById("nextStage").style.display = "block";
-    document.getElementById("endScore").style.display = "none";
+
+function updateButtons() {
+    if (game.current_stage == game.players[0].list_score.length) {
+        document.getElementById("nextStage").style.display = "none";
+        document.getElementById("endScore").style.display = "block";
+    } else {
+        document.getElementById("nextStage").style.display = "block";
+        document.getElementById("endScore").style.display = "none";
+    }
 }
+
+
 
 
 
